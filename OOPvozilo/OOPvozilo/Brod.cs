be0aad7ceb4b28640b4f7fa13281cc35502105aa
markoknezovic <58472152+markoknezovic@users.noreply.github.com>
2021-10-18 10:22:56 +0300

    Add files via upload

---
+++ OOPvozilo/OOPvozilo/Brod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPvozilo
+{
+    class Brod : Vozilo
+    {
+        public Brod() {
+            JeLiLeti = false; JeLiPlovi = true;
+        }
+    }
+}
---
+++ OOPvozilo/OOPvozilo/Vozilo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPvozilo
+{
+    class Vozilo
+
+    {
+        protected bool JeLiLeti;
+        protected bool JeLiPlovi;
+        
+        public void KudaVozi()
+        { Console.WriteLine("{0} {1}", JeLiLeti, JeLiPlovi);
+        }
+    
+}
+}
